19e08d68047513f45609ebeab1b0ca368e6c13bf
Dave Senn <dave.jon.senn@gmail.com>
2022-12-10 00:02:12 +0100

    .

---
+++ ModernWebApi/Program.cs
@@ -0,0 +1,18 @@
+using System.Net.NetworkInformation;
+using ConfigurationPlaceholders;
+
+var ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+var fullDomainName = ipProperties.HostName.ToLower();
+if ( !String.IsNullOrWhiteSpace( ipProperties.DomainName ) )
+    fullDomainName = $"{ipProperties.HostName}.{ipProperties.DomainName}".ToLower();
+
+var builder = WebApplication.CreateBuilder( args );
+builder
+    .AddConfigurationPlaceholders( new InMemoryPlaceholderResolver( new Dictionary<String, String?>
+    {
+        { "FQDN", fullDomainName }
+    } ) );
+
+var app = builder.Build();
+app.MapGet( "/GetCertInfo", ( IConfiguration configuration ) => $"Use certificate with subject {configuration["CertificateSubject"]} for HTTPS connection." );
+app.Run();
---
+++ examples/HostBuilderWebApi/Program.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using ConfigurationPlaceholders;
+
+public static class Program
+{
+    public static void Main( String[] args )
+    {
+        CreateHostBuilder( args )
+            .Build()
+            .Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder( String[] args )
+    {
+        var framework = Assembly
+                        .GetEntryAssembly()?
+                        .GetCustomAttribute<TargetFrameworkAttribute>()?
+                        .FrameworkName;
+
+        return Host
+               .CreateDefaultBuilder( args )
+               .AddConfigurationPlaceholders( new InMemoryPlaceholderResolver( new Dictionary<String, String?>
+               {
+                   { "Framework", framework },
+                   { "OsInfo", RuntimeInformation.OSDescription },
+               } ) )
+               .ConfigureWebHostDefaults( webBuilder => { webBuilder.UseStartup<Startup>(); } );
+    }
+}
